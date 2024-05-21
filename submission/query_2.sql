4a91ec2c85a8821f78d7977fa650f2b0d609d5b9
Ameena Ansari <ameena5432@gmail.com>
2024-05-21 14:51:44 -0700

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,15 @@
+-- Write a DDL statement to create a cumulating user activity table by device.
+
+
+CREATE OR REPLACE TABLE user_devices_cumulated
+(
+        user_id BIGINT,             -- User identfier
+        browser_type VARCHAR,       -- Browser Type of the user used for this activity
+        dates_active ARRAY(date),   -- Date of the user being active 
+        date DATE                   -- Current date
+)
+WITH
+(
+    partitioning = Array['date']    -- Partitioning is good on Date
+)
+
