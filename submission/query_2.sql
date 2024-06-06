46dfdb01c3d7a04824775ded5da3e02d10b972bf
Ameena Ansari <ameena5432@gmail.com>
2024-06-06 16:19:09 -0700

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,7 +1,7 @@
 -- Write a DDL statement to create a cumulating user activity table by device.
 
 
-CREATE OR REPLACE TABLE user_devices_cumulated_new
+CREATE OR REPLACE TABLE ameena543246912.user_devices_cumulated_new
 (
         user_id BIGINT,             -- User identfier
         browser_type VARCHAR,       -- Browser Type of the user used for this activity
